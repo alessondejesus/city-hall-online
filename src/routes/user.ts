2be4b159c7eb3c5602d8853d5685be2679d197c3
alessondejesus <asn9006@hotmail.com>
2020-09-06 23:39:06 -0300

    lerning all from test

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,19 +3,7 @@ const router = Router()
 
 router
     .get('/', (req, res) => {
-        type UserInfo = {
-            id: number
-            name: string
-            email: string
-        }
-        type ProfessionInfo = {
-            accountUser: UserInfo
-            id: number
-            name: string
-        }
-        let account: UserInfo = { id: 5, name: 'Áleson de Jesus', email: 'asn90062@hotmail.com' }
-        let char: ProfessionInfo = { accountUser: account, id: 555, name: "Developer" }
-        res.json({ account, char })
+        res.json({ message: 'hello!' })
     })
     .post('/', (req, res) => {
         res.json({ message: 'post endpoint of user' })
--- tests/hello.test.ts
+++ tests/hello.test.ts
@@ -1,3 +1,17 @@
+type UserInfo = {
+    id: number
+    name: string
+    email: string
+}
+type ProfessionInfo = {
+    accountUser: UserInfo
+    id: number
+    name: string
+}
+let account: UserInfo = { id: 5, name: 'Áleson de Jesus', email: 'asn90062@hotmail.com' }
+let char: ProfessionInfo = { accountUser: account, id: 555, name: "Developer" }
+console.log(char)
+
 function sum(a:number, b:number) {
     return a + b;
 }
